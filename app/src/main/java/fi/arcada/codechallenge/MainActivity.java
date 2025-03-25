89fbb6a4248c4d1af8dd58cb5e82e751d8f26972
kolehmas@arcada.fi <kolehmas@arcada.fi>
2025-03-25 16:08:30 +0200

    Test Commit

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -8,16 +8,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.widget.TextView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         TextView textView = findViewById(R.id.helloworldmessage);
 
         textView.setText("My app works!");
+
+        ArrayList<Double> myDoubleArray = new ArrayList<Double>();
+        myDoubleArray.add(2.1);
+        myDoubleArray.add(4.2);
+        myDoubleArray.add(6.3);
+        myDoubleArray.add(8.4);
+        myDoubleArray.add(10.5);
+        System.out.println(myDoubleArray);
+
+
     }
 }
